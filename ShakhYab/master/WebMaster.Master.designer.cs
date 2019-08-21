804237eb1dbdb499e467f4756560e9337af2a247
unknown <mohammadi.ehsan1994@gmail.com>
2019-08-22 03:14:53 +0430

    Add bottom menu

--- ShakhYab/master/WebMaster.Master.designer.cs
+++ ShakhYab/master/WebMaster.Master.designer.cs
@@ -31,6 +31,24 @@ namespace ShakhYab.master {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder main;
         
         /// <summary>
+        /// footer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder footer;
+        
+        /// <summary>
+        /// menuBottom control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder menuBottom;
+        
+        /// <summary>
         /// header control.
         /// </summary>
         /// <remarks>
@@ -47,14 +65,5 @@ namespace ShakhYab.master {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button buttonStartShakhyab;
-        
-        /// <summary>
-        /// footer control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder footer;
     }
 }
